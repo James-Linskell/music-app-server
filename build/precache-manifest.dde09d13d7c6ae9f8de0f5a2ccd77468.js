b7a23152e94c93fd04be6988e9efeffdf2f89ee4
jamie <rjb19179@uni.strath.ac.uk>
2020-07-03 20:50:58 +0100

    Added routes for serving the react app and my API in the same project.

---
+++ build/precache-manifest.dde09d13d7c6ae9f8de0f5a2ccd77468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30de64c8f02c82f31853c768461c823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10bb44c38bd7c296b91f",
+    "url": "/static/css/main.be4203c6.chunk.css"
+  },
+  {
+    "revision": "67e8f391e601a8266c48",
+    "url": "/static/js/2.de69e116.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de69e116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bb44c38bd7c296b91f",
+    "url": "/static/js/main.98459e0a.chunk.js"
+  },
+  {
+    "revision": "86acefc8fd8dfa9b480e",
+    "url": "/static/js/runtime-main.2d894620.js"
+  },
+  {
+    "revision": "aede5c8913f06bc76ee37fb1b192df44",
+    "url": "/static/media/hellify.aede5c89.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = "3000";
+const port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/build'));
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
