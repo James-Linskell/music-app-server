aef08dc0e032348315fb3c5acd776c2513644932
jamie <jhdlinskell@protonmail.com>
2020-07-22 19:09:42 +0100

    Updated to fully working v1.

---
+++ build/precache-manifest.636037dc11485de373235c71d95ef3eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2eb3ff837aea1c5089da4710494d5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0089e7a5b46a5c81e9f3",
+    "url": "/static/css/main.635549a3.chunk.css"
+  },
+  {
+    "revision": "cdae8c5e4739c67d4786",
+    "url": "/static/js/2.56a686df.chunk.js"
+  },
+  {
+    "revision": "dfeca4c03bfa1615c42db68b7e95d1c8",
+    "url": "/static/js/2.56a686df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0089e7a5b46a5c81e9f3",
+    "url": "/static/js/main.5256f005.chunk.js"
+  },
+  {
+    "revision": "86acefc8fd8dfa9b480e",
+    "url": "/static/js/runtime-main.2d894620.js"
+  },
+  {
+    "revision": "aede5c8913f06bc76ee37fb1b192df44",
+    "url": "/static/media/hellify.aede5c89.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.15ee9f833729b95bf50aa7954bf0aaee.js"
+  "/precache-manifest.636037dc11485de373235c71d95ef3eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
